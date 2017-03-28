1e04c254a8f1c7695f22c446305204df0e573128
Gede Lumbung <gedesumawijaya@gmail.com>
2017-03-29 00:00:42 +0800

    python : basic selection

---
+++ basic/basic_elif.py
@@ -0,0 +1,8 @@
+gender = "male"
+if(gender == "male"):
+	print("You are man")
+elif(gender == "female"):
+	print("You are woman")
+else:
+	print("You are shemale")
+
---
+++ basic/basic_if.py
@@ -0,0 +1,6 @@
+man = True
+if(man):
+	print("You are man")
+else:
+	print("You are woman")
+
